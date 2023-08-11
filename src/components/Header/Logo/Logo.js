fc1eded07d1d22f27b6830b11131b4a75e0f767d
benjamin verdès <verdesb.devacc@gmail.com>
2023-08-11 16:27:46 +0200

    fix src logo

--- src/components/Header/Logo/Logo.js
+++ src/components/Header/Logo/Logo.js
@@ -1,7 +1,7 @@
 export const Logo = () => {
     return (
         <div class="header__containerLogo">
-            <a href="/" class="header__linkLogo"><img class="header__logo" src="../../../assets/icons/search.svg" alt=""></img></a>
+            <a href="/" class="header__linkLogo"><img class="header__logo" ></img></a>
         </div>
 
 
