bac780a0b804b0c150b3a6a1714973a51ba67960
benjamin verdès <verdesb.devacc@gmail.com>
2023-08-11 15:46:57 +0200

    src fix 2

--- src/components/Header/Logo/Logo.js
+++ src/components/Header/Logo/Logo.js
@@ -1,7 +1,7 @@
 export const Logo = () => {
     return (
         <div class="header__containerLogo">
-            <a href="/" class="header__linkLogo"><img class="header__logo" src="../../../static/assets/icons/logo.svg" alt=""></img></a>
+            <a href="/" class="header__linkLogo"><img class="header__logo" src="../../static/assets/icons/logo.svg" alt=""></img></a>
         </div>
 
 
