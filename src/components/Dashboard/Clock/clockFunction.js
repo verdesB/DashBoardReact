b603f9aef3ebc99e758201e5ad6dcf20bb7b2ad7
benjamin verdès <verdesb.devacc@gmail.com>
2023-08-11 18:50:50 +0200

    fix dateNumber

--- src/components/Dashboard/Clock/clockFunction.js
+++ src/components/Dashboard/Clock/clockFunction.js
@@ -4,6 +4,7 @@ export const updateClock = () => {
     const minutes = now.getMinutes();
     const seconds = now.getSeconds();
     const day = now.toLocaleString('fr-FR', { weekday: 'long' });
+    const dayNumber = now.getDate();
     const month = now.toLocaleString('fr-FR', { month: 'long' });
     const year = now.getFullYear();
 
@@ -15,7 +16,7 @@ export const updateClock = () => {
     hoursContainer.textContent = hours.toString().padStart(2, '0');
     minutesContainer.textContent = minutes.toString().padStart(2, '0');
     secondsContainer.textContent = seconds.toString().padStart(2, '0');
-    dateContainer.textContent = `${day} ${month} ${year}`;
+    dateContainer.textContent = `${day} ${dayNumber} ${month} ${year}`;
 }
 
 setInterval(updateClock, 1000);
